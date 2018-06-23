788e4427251bfbf9cf61472df22fdc85c90ced64
Diogo Cró <diogocror@gmail.com>
2018-06-23 20:36:50 +0100

    first commit

---
+++ HelloWorld/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloWorld
+{
+    [Activity(Label = "HelloWorld", MainLauncher = true)]
+    public class MainActivity : Activity
+        {
+            protected override void OnCreate(Bundle savedInstanceState)
+            {
+                base.OnCreate(savedInstanceState);
+
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.Main);
+
+                var button = FindViewById<Button>(Resource.Id.btn);
+                var text = FindViewById<TextView>(Resource.Id.txt);
+            
+                button.Click += (s, o) => { text.Text = "Hello world"; };
+
+            }
+    }
+}
+
